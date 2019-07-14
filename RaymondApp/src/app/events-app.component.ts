533fdb3f7d6a352176df749e966a58ba7504e9b4
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-07-14 22:11:44 +0300

    addedImage

--- RaymondApp/src/app/events-app.component.ts
+++ RaymondApp/src/app/events-app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: '<h2>Hello World</h2>'  
+  template: '<h2>Hello World</h2><img src="/assets/images/basic-shield.png"/>'
 })
 export class EventsAppComponent {
   title = 'RaymondApp';
