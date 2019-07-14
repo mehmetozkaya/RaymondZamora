2581e6d488ce57662b94f51313f5b901c1370305
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-07-14 17:58:23 +0300

    configs

--- RaymondZamora/Startup.cs
+++ RaymondZamora/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RaymondZamora.Data;
 
 namespace RaymondZamora
 {
@@ -25,6 +26,9 @@ namespace RaymondZamora
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CampContext>();
+            services.AddScoped<ICampRepository, CampRepository>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
