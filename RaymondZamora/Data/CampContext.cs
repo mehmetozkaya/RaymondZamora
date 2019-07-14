2b44bcc3801ff6ec5d75ff0b84278492ca4efa7a
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-07-14 16:11:01 +0300

    CampContext

---
+++ RaymondZamora/Data/CampContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RaymondZamora.Data
+{
+    public class CampContext : DbContext
+    {
+        private readonly IConfiguration _config;
+
+        public CampContext(DbContextOptions options, IConfiguration config) : base(options)
+        {
+            _config = config ?? throw new ArgumentNullException(nameof(config));
+        }
+
+
+    }
+}
