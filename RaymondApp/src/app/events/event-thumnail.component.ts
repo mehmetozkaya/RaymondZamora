d8f41d752bd5fd9d80f4a7df7d56add4876f4b00
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-08-27 14:29:22 +0300

    EventThumnailComponent

---
+++ RaymondApp/src/app/events/event-thumnail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+        selector: 'event-thumnail',
+        templateUrl: './event-thumnail.component.html'
+})
+export class EventThumnailComponent {
+    event: any;
+}
