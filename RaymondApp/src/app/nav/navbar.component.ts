e2cd2350e3f4748901446738e70f85005ad3825d
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-08-28 13:53:21 +0300

    navbar html added

--- RaymondApp/src/app/nav/navbar.component.ts
+++ RaymondApp/src/app/nav/navbar.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'nav-bar'
+    selector: 'nav-bar',
+    templateUrl: './nav-bar.component.html'
 })
 export class NavBarComponent {
 
