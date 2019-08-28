2df64abb8b24fbd116cbcf48d5898edb42d2b052
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-08-28 18:36:58 +0300

    toastr 3rd party added

--- RaymondApp/src/app/events/events-list.component.ts
+++ RaymondApp/src/app/events/events-list.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
+
+declare let toastr
 
 @Component({
     selector: 'events-list',
     templateUrl: './events-list.component.html'
 })
-export class EventListComponent {    
+export class EventListComponent implements OnInit {    
     events: any[]
-    constructor(private eventService : EventService){
-      this.events = this.eventService.getEvents();
+    constructor(private eventService : EventService){      
+    }
+
+    ngOnInit() {
+        this.events = this.eventService.getEvents();
     }
     
     handleEventClicked(data) : void {
         console.log('received data : '+ data);
-    }    
+    }
+
+    handleThumbnailClick(eventName) {
+        toastr.success(eventName);
+    }
 }
