ec6e2b20ac3c5d8de022b5ff85dc304dcf0ffac9
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-08-29 20:30:16 +0300

    ngSubmit

---
+++ RaymondApp/src/app/user/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './login.component.html'
+})
+export class LoginComponent {
+    userName
+    password
+    
+    login(formValues) {
+        console.log(formValues)
+    }
+
+}
