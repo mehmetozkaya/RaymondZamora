99d0e4277a5335236e625a59b5a4de45aa70a65b
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-08-29 19:03:05 +0300

    index exports

--- RaymondApp/src/app/app.module.ts
+++ RaymondApp/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {
+  EventListComponent,
+  EventThumnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator  
+} from './events/index'
+
 import { EventsAppComponent } from './events-app.component';
-import { EventListComponent } from './events/events-list.component';
-import { EventThumnailComponent } from './events/event-thumnail.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
-import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
+
 
 @NgModule({ 
   imports: [
---
+++ RaymondApp/src/app/events/index.ts
@@ -0,0 +1,5 @@
+export * from './create-event.component'
+export * from './event-thumnail.component'
+export * from './events-list.component'
+export * from './shared/index'
+export * from './event-details/index'
