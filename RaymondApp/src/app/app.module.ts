f2f4f23815dc702070a7f5c21c06987336f6b968
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-08-29 11:14:41 +0300

    RouterModule added

--- RaymondApp/src/app/app.module.ts
+++ RaymondApp/src/app/app.module.ts
@@ -8,10 +8,13 @@ import { NavBarComponent } from './nav/navbar.component';
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 @NgModule({ 
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     EventsAppComponent,
--- RaymondApp/src/app/routes.ts
+++ RaymondApp/src/app/routes.ts
@@ -1,7 +1,9 @@
+import { Routes } from '@angular/router';
 import { EventListComponent } from './events/events-list.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
 
-export const appRoutes = [
+export const appRoutes: Routes = [
     { path: 'events', component: EventListComponent},
-    { path: 'events/:id', component: EventDetailsComponent}
+    { path: 'events/:id', component: EventDetailsComponent},
+    { path: '', redirectTo: '/events', pathMatch: 'full'}    
 ]
