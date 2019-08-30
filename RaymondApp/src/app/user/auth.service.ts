41c173db778840f666788eae9173d9d81df0aeac
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-08-30 12:25:14 +0300

    AuthService

---
+++ RaymondApp/src/app/user/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { IUser } from './user.model';
+
+
+@Injectable()
+export class AuthService {
+    currentUser: IUser
+    loginUser(userName: string, password: string) {
+        this.currentUser = {
+            id: 1,
+            userName: userName,
+            firstName: 'Mehmet',
+            lastName: 'Ozkaya'
+        }
+    }
+}
