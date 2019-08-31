db8721269318d4b8a898bcd739b08fc5b4302a2d
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-08-31 19:53:28 +0300

    sessionlist component added

--- RaymondApp/src/app/events/create-event.component.ts
+++ RaymondApp/src/app/events/create-event.component.ts
@@ -8,16 +8,18 @@ import { EventService } from './shared';
 export class CreateEventComponent {
 
     newEvent
+    isDirty : boolean = true
 
     constructor(private router: Router, private eventService: EventService){
     }
 
     saveEvent(formValues) {
         this.eventService.saveEvent(formValues)
-        this.router.navigate(['/events']);
+        this.isDirty = false
+        this.router.navigate(['/events'])
     }
 
     cancel(): void {
-        this.router.navigate(['/events']);
+        this.router.navigate(['/events'])
     }
 }
---
+++ RaymondApp/src/app/events/session-list.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { ISession } from './shared';
+
+
+@Component({
+    selector: 'session-list',
+    templateUrl: './session-list.component.html'
+})
+export class SessionListComponent {
+    @Input() sessions : ISession[]
+}
