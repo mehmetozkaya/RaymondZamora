3d735524506e684fc70a76cea832cc286bb9fc0d
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-09-01 16:13:24 +0300

    CollapsibleWellComponent

---
+++ RaymondApp/src/app/common/collapsible-well.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'collapsible-well',
+    templateUrl: 'collapsible-well.component.html'
+})
+export class CollapsibleWellComponent {
+
+}
