5e97fced7c4431e4a766b0d91d378bc8bdb9b7f5
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-09-14 23:11:45 +0300

    foundSessions

--- RaymondApp/src/app/nav/navbar.component.ts
+++ RaymondApp/src/app/nav/navbar.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../user/auth.service';
+import { ISession } from '../events';
 
 @Component({
     selector: 'nav-bar',
     templateUrl: './navbar.component.html'
 })
 export class NavBarComponent {
+    searchTerm: string = ""
+    foundSessions: ISession[]
+    
     constructor(private authService: AuthService){        
+    }
+
+    searchSessions(searchTerm): void {
+        
     }
 }
