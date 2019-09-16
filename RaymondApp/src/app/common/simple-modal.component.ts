3f37b236d184b52a3c94426fe7c43d65971945d4
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-09-16 09:48:27 +0300

    simple-modal html added

--- RaymondApp/src/app/common/simple-modal.component.ts
+++ RaymondApp/src/app/common/simple-modal.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'simple-modal',
     template: `
+    <div id="simple-modal" class="modal fade" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
+                    <h4 class="modal-title">{{title}}</h4>
+                </div>
+                <div class="modal.body">
+                    <ng-content></ng-content>
+                </div>
+            </div>
+        </div>
+    </div>
     `,
-    styles: [``]
+    styles: [`
+        .modal-body { height: 250px; overflow-y: scroll; }
+    `]
 })
 export class SimpleModalComponent {
+    @Input() title: string
 
 }
