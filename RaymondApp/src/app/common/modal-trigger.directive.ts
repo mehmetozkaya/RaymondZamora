7a955af9b5cb345a421845b172ca3bf8f8f0c559
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-09-16 10:57:29 +0300

    ElementRef

--- RaymondApp/src/app/common/modal-trigger.directive.ts
+++ RaymondApp/src/app/common/modal-trigger.directive.ts
@@ -1,17 +1,19 @@
-import { Directive, OnInit, Inject } from '@angular/core';
+import { Directive, OnInit, Inject, ElementRef } from '@angular/core';
 import { JQ_TOKEN } from './jQuery.service';
 
 
 @Directive({
     selector: '[modal-trigger]'
 })
-export class ModalTriggerDirective implements OnInit {
-   
-    constructor(@Inject(JQ_TOKEN) private $ : any) {        
+export class ModalTriggerDirective implements OnInit {   
+    private el: HTMLElement
+
+    constructor(ref: ElementRef, @Inject(JQ_TOKEN) private $ : any) {
+        this.el = ref.nativeElement
     }
     
     ngOnInit(): void {
-        this.$('#simple-modal').modal({})
+        this.el.addEventListener('click', e => { this.$('#simple-modal').modal({}) })
     }
 
 }
