4e3675e275c085c2bba5714ca3a0d9ee92a803f5
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-09-17 09:43:24 +0300

    currentUser undefined

--- RaymondApp/src/app/user/auth.service.ts
+++ RaymondApp/src/app/user/auth.service.ts
@@ -37,6 +37,7 @@ export class AuthService {
     }
 
     logout() {
+        this.currentUser = undefined
         let options = { headers: new HttpHeaders({'Content-Type': 'application/json'})}
         return this.http.post('/api/logout', {}, options)
     }
